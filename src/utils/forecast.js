43a5930b1122895cf39913a34ef765faee05b23c
Anil <ank.dot.999@gmail.com>
2019-09-13 02:08:11 +0530

    initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request=require('request')
+
+const forecast=(latitude,longitude,callback)=>{
+
+    const url='https://api.darksky.net/forecast/dcf2144b5409cd2943ecdff67352a0b8/'+latitude+','+longitude+'?units=si'
+
+request({url:url,json:true},(error,response)=>{
+    
+    if (error){
+        callback('unable to connect to location services',undefined)
+    }else if(response.body.error){
+        callback('unable to find the location.Try another search',undefined)
+    }
+    
+    else{
+        callback(undefined,'The Current temp is '+response.body.currently.temperature)
+    }
+
+})
+
+
+
+}
+
+
+module.exports=forecast
